2d018231d73ed3a82bef8f57299492cd60a8913f
Harshana Praveen <harshana6681@gmail.com>
2026-06-28 17:48:35 +0530

    Created Folder Structure and core setup

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="min-h-screen flex items-center justify-center">
+      <h1 className="text-2xl font-bold">Smart Resume Reviewer</h1>
+    </main>
+  );
+}
